5f4ada825d00fca6276a5b80006986e717fc9f95
2312323123 <2312323@protonmail.com>
2022-09-12 16:01:15 +0200

    Updates

---
+++ component---src-pages-about-js-3a82cb4d151327ff74c7.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkbitehack=self.webpackChunkbitehack||[]).push([[682],{4633:function(t,e,a){a.r(e);var n=a(7294),l=a(1597);e.default=function(t){var e=t.location;e&&console.log(e);var a=e.state.data?e.state.tak:"fdsa";return n.createElement(n.Fragment,null,n.createElement("p",null,"I'm making this by following the Gatsby Tutorial."," ",a?a.toString():"nie"),n.createElement(l.rU,{to:"/"},"Index"))}}}]);
+//# sourceMappingURL=component---src-pages-about-js-3a82cb4d151327ff74c7.js.map
